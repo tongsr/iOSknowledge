e25e77f8350203c84b6774ff11bbe4d9549d17de
franky <493860286@qq.com>
2019-07-21 22:21:34 +0800

    update coreAnimation

--- iOSKnowledgeTrain/iOSKnowledgeTrain/coreAnimation/CoreAnimationVC.m
+++ iOSKnowledgeTrain/iOSKnowledgeTrain/coreAnimation/CoreAnimationVC.m
@@ -8,7 +8,7 @@
 
 #import "CoreAnimationVC.h"
 
-@interface CoreAnimationVC ()
+@interface CoreAnimationVC ()<CAAnimationDelegate>
 - (IBAction)baseMove:(id)sender;
 - (IBAction)animation0:(id)sender;
 - (IBAction)animation1:(id)sender;
@@ -36,6 +36,21 @@
     
     
 - (IBAction)animation0:(id)sender {
+    CABasicAnimation *basicAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+    basicAnimation.delegate=self;
+    //2.设置动画属性初始值和结束值
+    //basicAnimation.fromValue=[NSNumber numberWithInteger:50];//可以不设置，默认为图层初始状态
+    basicAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(80, 100)];
+    //存储当前位置在动画结束后使用
+    [basicAnimation setValue:[NSValue valueWithCGPoint:CGPointMake(80, 100)] forKey:@"KCBasicAnimationLocation"];
+    
+    //设置其他动画属性
+    basicAnimation.duration = 1.0f;//动画时间5秒
+    // basicAnimation.repeatCount=HUGE_VALF;//设置重复次数,HUGE_VALF可看做无穷大，起到循环动画的效果
+    //basicAnimation.removedOnCompletion = NO;//运行一次是否移除动画
+    
+    //3.添加动画到图层，注意key相当于给动画进行命名，以后获得该动画时可以使用此名称获取
+    [self.testview.layer addAnimation:basicAnimation forKey:@"KCBasicAnimation_Translation"];
 }
     
     
@@ -51,5 +66,21 @@
 }
     
     
+-(void)animationDidStart:(CAAnimation *)anim{
     
+}
+    
+    
+    
+-(void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag{
+    //开启事务
+    [CATransaction begin];
+    //禁用隐式动画
+    [CATransaction setDisableActions:YES];
+    
+    self.testview.layer.position=[[anim valueForKey:@"KCBasicAnimationLocation"] CGPointValue];
+    
+    //提交事务
+    [CATransaction commit];
+}
 @end
